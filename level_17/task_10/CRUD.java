a595d7fbf0bf5653c91d87dd539b7d28f36e6279
umisaou <47829295+umisaou@users.noreply.github.com>
2019-11-21 15:51:08 +0300

    Create CRUD.java

---
+++ level_17/task_10/CRUD.java
@@ -0,0 +1,72 @@
+package com.javarush.task.task17.task1710;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class CRUD {
+    private static int create(String name, Sex sex, Date date) {
+        if (sex == Sex.MALE) {
+            Solution.allPeople.add(Person.createMale(name, date));
+        } else
+            Solution.allPeople.add(Person.createFemale(name, date));
+        return Solution.allPeople.size() - 1;
+    }
+
+    private static void update(int id, String name, Sex sex, Date date) {
+        Solution.allPeople.get(id).setName(name);
+        Solution.allPeople.get(id).setSex(sex);
+        Solution.allPeople.get(id).setBirthDate(date);
+    }
+
+    private static void delete(int id) {
+        Solution.allPeople.get(id).setName(null);
+        Solution.allPeople.get(id).setSex(null);
+        Solution.allPeople.get(id).setBirthDate(null);
+    }
+
+    private static String inform(int id) {
+        String information = "";
+        information += Solution.allPeople.get(id).getName() + " ";
+        if (Solution.allPeople.get(id).getSex() == Sex.MALE) {
+            information += "м" + " ";
+        } else
+            information += "ж" + " ";
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+        information += sdf.format(Solution.allPeople.get(id).getBirthDate());
+        return information;
+
+    }
+
+    public static void work(String[] s) throws Exception {
+        int id;
+        String name = "";
+        Sex sex;
+        Date date;
+        if (s[0].equals("-c")) {
+
+            name = s[1];
+            if (s[2].equals("м")) {
+                sex = Sex.MALE;
+            } else
+                sex = Sex.FEMALE;
+            date = new SimpleDateFormat("dd/MM/yyyy").parse(s[3]);
+            System.out.println(create(name, sex, date));
+        } else if (s[0].equals("-u")) {
+            id = Integer.parseInt(s[1]);
+            name = s[2];
+            if (s[3].equals("м")) {
+                sex = Sex.MALE;
+            } else
+                sex = Sex.FEMALE;
+            date = new SimpleDateFormat("dd/MM/yyyy").parse(s[4]);
+            update(id, name, sex, date);
+        } else if (s[0].equals("-d")) {
+            id = Integer.parseInt(s[1]);
+            delete(id);
+        } else if (s[0].equals("-i")) {
+            id = Integer.parseInt(s[1]);
+            System.out.println(inform(id));
+        }
+    }
+}
