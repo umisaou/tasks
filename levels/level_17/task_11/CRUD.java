f1a9d11bfe28883d491574a038b16cc6e31c5b4c
umisaou <47829295+umisaou@users.noreply.github.com>
2019-11-28 19:55:06 +0300

    Update CRUD.java

--- levels/level_17/task_11/CRUD.java
+++ levels/level_17/task_11/CRUD.java
@@ -40,47 +40,56 @@ public class CRUD {
     }
 
     public static void work(String[] s) throws Exception {
-        int numberOfParam;
+        int numberOfParam = s.length - 1;
         int id;
         String name = "";
         Sex sex;
         Date date;
-        if (s[0].equals("-c")) {
-            numberOfParam = (s.length - 1) / 3;
-            for (int i = 0; i < numberOfParam; i++) {
-                name = s[1];
-                if (s[2].equals("м")) {
-                    sex = Sex.MALE;
-                } else
-                    sex = Sex.FEMALE;
-                date = new SimpleDateFormat("dd/MM/yyyy").parse(s[3]);
-                System.out.println(create(name, sex, date));
-            }
-
-        } else if (s[0].equals("-u")) {
-            numberOfParam = (s.length - 1) / 4;
-            for (int i = 0; i < numberOfParam; i++) {
-                id = Integer.parseInt(s[1]);
-                name = s[2];
-                if (s[3].equals("м")) {
-                    sex = Sex.MALE;
-                } else
-                    sex = Sex.FEMALE;
-                date = new SimpleDateFormat("dd/MM/yyyy").parse(s[4]);
-                update(id, name, sex, date);
-            }
-        } else if (s[0].equals("-d")) {
-            numberOfParam = s.length - 1;
-            for (int i = 0; i < numberOfParam; i++) {
-                id = Integer.parseInt(s[1]);
-                delete(id);
-            }
-        } else if (s[0].equals("-i")) {
-            numberOfParam = s.length - 1;
-            for (int i = 0; i < numberOfParam; i++) {
-                id = Integer.parseInt(s[1]);
-                System.out.println(inform(id));
-            }
+        switch (s[0]) {
+            case "-c":
+                numberOfParam = (s.length - 1) / 3;
+                for (int i = 0; i < numberOfParam; i++) {
+                    name = s[1 + i * 3];
+                    if (s[2 + i * 3].equals("м")) {
+                        sex = Sex.MALE;
+                    } else
+                        sex = Sex.FEMALE;
+                    date = new SimpleDateFormat("dd/MM/yyyy").parse(s[3 + i * 3]);
+                    synchronized (Solution.allPeople) {
+                        System.out.println(create(name, sex, date));
+                    }
+                }
+                break;
+            case "-u":
+                numberOfParam = (s.length - 1) / 4;
+                for (int i = 0; i < numberOfParam; i++) {
+                    id = Integer.parseInt(s[1 + i * 4]);
+                    name = s[2 + i * 4];
+                    if (s[3 + i * 4].equals("м")) {
+                        sex = Sex.MALE;
+                    } else
+                        sex = Sex.FEMALE;
+                    date = new SimpleDateFormat("dd/MM/yyyy").parse(s[4 + i * 4]);
+                    synchronized (Solution.allPeople) {
+                        update(id, name, sex, date);
+                    }
+                }
+                break;
+            case "-d":
+                for (int i = 0; i < numberOfParam; i++) {
+                    id = Integer.parseInt(s[1 + i]);
+                    synchronized (Solution.allPeople) {
+                        delete(id);
+                    }
+                }
+                break;
+            case "-i":
+                for (int i = 0; i < numberOfParam; i++) {
+                    id = Integer.parseInt(s[1 + i]);
+                    synchronized (Solution.allPeople) {
+                        System.out.println(inform(id));
+                    }
+                }
         }
     }
 }
