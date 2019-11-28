6a4b6337e4ec92dfc7d10167fb89fadba166cf44
umisaou <47829295+umisaou@users.noreply.github.com>
2019-11-28 20:48:54 +0300

    Update Solution.java

--- levels/level_18/task_25/Solution.java
+++ levels/level_18/task_25/Solution.java
@@ -10,21 +10,21 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
-        HashMap<Integer, String> map = new HashMap<>();
-        String someName = "";
+        HashMap<Integer, String> partsOfFile = new HashMap<>();
+        String resultFileName = "";
         while (true) {
             String filename = consoleReader.readLine();
             if (filename.equals("end"))
                 break;
 
             String partsOfName[] = filename.split("\\.part");
-            someName = partsOfName[0];
-            map.put(Integer.parseInt(partsOfName[1]), filename);
+            resultFileName = partsOfName[0];
+            partsOfFile.put(Integer.parseInt(partsOfName[1]), filename);
         }
 
-        BufferedOutputStream fileWriter = new BufferedOutputStream(new FileOutputStream(someName, true));
-        for (int i = 1; i <= map.size(); i++) {
-            BufferedInputStream fileReader = new BufferedInputStream(new FileInputStream(map.get(i)));
+        BufferedOutputStream fileWriter = new BufferedOutputStream(new FileOutputStream(resultFileName, true));
+        for (int i = 1; i <= partsOfFile.size(); i++) {
+            BufferedInputStream fileReader = new BufferedInputStream(new FileInputStream(partsOfFile.get(i)));
             byte[] buffer = new byte[fileReader.available()];
             fileReader.read(buffer);
             fileWriter.write(buffer);
