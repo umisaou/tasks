698c2903363e70e99b299cc7d814f21a9ce9b098
umisaou <47829295+umisaou@users.noreply.github.com>
2019-12-05 12:09:40 +0300

    Update Solution.java

--- levels/level_22/task_09/Solution.java
+++ levels/level_22/task_09/Solution.java
@@ -15,18 +15,17 @@ public class Solution {
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedReader fileReader = new BufferedReader(new FileReader(consoleReader.readLine()));
         consoleReader.close();
-        ArrayList<String> words = new ArrayList<>();
+        String [] words = null;
         while (true) {
             String s = fileReader.readLine();
             if (s == null)
                 break;
-            words.addAll(Arrays.asList(s.split(" ")));
+            words = s.split(" ");
         }
-        fileReader.close();
-        String[] sWords = new String[words.size()];
-        words.toArray(sWords);
 
-        StringBuilder result = getLine(sWords);
+        fileReader.close();
+
+        StringBuilder result = getLine(words);
         System.out.println(result.toString());
     }
 
@@ -34,19 +33,19 @@ public class Solution {
         if (words.length < 1)
             return new StringBuilder();
         StringBuilder result = null;
-        for (int j = 0; ; j++) {
+        for (int i = 0; ; i++) {
             ArrayList<String> list = new ArrayList<>(Arrays.asList(words));
-            String symbol = list.get(j).charAt(list.get(j).length() - 1) + "";
-            result = new StringBuilder(list.get(j));
-            list.remove(j);
+            String symbol = list.get(i).charAt(list.get(i).length() - 1) + "";
+            result = new StringBuilder(list.get(i));
+            list.remove(i);
             boolean isEnd = false;
             while (!isEnd) {
                 isEnd = true;
-                for (int i = 0; i < list.size(); i++) {
-                    if ((list.get(i).charAt(0) + "").equalsIgnoreCase(symbol)) {
-                        result.append(" ").append(list.get(i));
-                        symbol = list.get(i).charAt(list.get(i).length() - 1) + "";
-                        list.remove(i);
+                for (int j = 0; j < list.size(); j++) {
+                    if ((list.get(j).charAt(0) + "").equalsIgnoreCase(symbol)) {
+                        result.append(" ").append(list.get(j));
+                        symbol = list.get(j).charAt(list.get(j).length() - 1) + "";
+                        list.remove(j);
                         isEnd = false;
                         break;
                     }
